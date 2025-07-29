d0d117c585bd80d91de863f8c3e321532143cc8a
Nived <nived@globalsurf.ae>
2025-07-29 12:13:22 +0530

    style:header

--- app/components/home/Index.tsx
+++ app/components/home/Index.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Banner from './sections/Banner'
+import Commitment from './sections/Commitment'
 
 const Index = () => {
   return (
-   <>
+   <div className='bg-black'>
     <Banner/>
-   </>
+    <Commitment/>
+   </div>
   )
 }
 
--- app/components/layout/Navbar.tsx
+++ app/components/layout/Navbar.tsx
@@ -1,14 +1,29 @@
 
 import React from 'react'
+import { menuItems } from './menuItems'
+import Image from 'next/image'
 
 const Navbar = () => {
   return (
-    <header className='fixed'>
-        <div className="container">
-            <div className='bg-white rounded-full shadow-md px-3 py-4 flex items-center justify-between'>
-                <div>
-                    Header
-                </div>
+    <header className='fixed w-full z-[100]'>
+        <div className="container w-full">
+            <div className='bg-white rounded-full shadow-md pr-[37px] py-[25px] flex items-center justify-between w-fit gap-10'>
+              <div className=''>
+                <Image src="/assets/logo-quad.png" alt="Logo" width={500} height={500} />
+              </div>
+                <ul className='flex w-full justify-between gap-5'>
+                  {menuItems.map((item)=>{
+                    return(
+                      <div className='flex flex-col group cursor-pointer'>
+                        <li className='text-nowrap'>{item.name}</li>
+                        <span className='bg-primary w-[0px] h-[2px] group-hover:w-full transition-all duration-300'></span>
+                      </div>
+                    )
+                  })}
+                </ul>
+                <button className='bg-primary text-white rounded-full py-[30px] px-[50px] text-nowrap cursor-pointer'>
+                  Contact Us
+                </button>
             </div>
         </div>
     </header>
