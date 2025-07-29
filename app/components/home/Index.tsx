cf8335dfa7552a8d5fb7d2fe347c3e8f028a1d38
Sreemayi-s-praseed <sreemayi@globalsurf.ae>
2025-07-29 17:34:33 +0530

    cta section created

--- app/components/home/Index.tsx
+++ app/components/home/Index.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Banner from './sections/Banner'
 import Commitment from './sections/Commitment'
 import Services from './sections/Services'
+import CTA from './sections/cta'
 
 const Index = () => {
   return (
@@ -9,6 +10,7 @@ const Index = () => {
     <Banner/>
     <Commitment/>
     <Services/>
+    <CTA/>
    </div>
   )
 }
