e881e2ee0789fbd6917b269aaef3e2c3c69f5105
rahees <raheez1995india@gmail.com>
2025-07-30 15:25:40 +0530

    commitment sec

--- app/components/home/sections/IndustriesList.tsx
+++ app/components/home/sections/IndustriesList.tsx
@@ -7,7 +7,7 @@ const IndustriesList = () => {
         <h2 className="text-80 leading-[1.125] mb-10 2xl:mb-50px">Industries We Serve</h2>
 
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-5 gap-[1px] relative">
-          <div className="absolute top-[50%] left-0 translate-y-[-50%] h-[0.5px] w-full bg-black z-40"></div>
+          <div className="absolute top-[50%] left-0 translate-y-[-50%] h-[0.5px] w-full bg-black z-40 hidden 2xl:block"></div>
           {homeData.industriesList.items.map((item, index) => (
             <div key={index} className="bg-theme-pink overflow-hidden rounded-2xl relative flex flex-col justify-between group">
               <div className="absolute top-0 left-0 w-full h-full bg-white z-0 opacity-0 group-hover:opacity-100 transition-all duration-300">
@@ -18,8 +18,8 @@ const IndustriesList = () => {
               <div className="absolute top-[50%] translate-y-[-50%] left-0 w-[51px] h-[51px] bg-white rounded-full
                z-40 opacity-0 group-hover:opacity-100 group-hover:left-[75%] transition-all duration-500 flex items-center justify-center">
                 <svg width="25" height="25" viewBox="0 0 25 25" fill="none" xmlns="http://www.w3.org/2000/svg">
-                  <path d="M14.9283 6.32861L21.0144 12.3986L14.9283 18.4686" stroke="#FF0000" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" />
-                  <path d="M3.96924 12.3979H20.8437" stroke="#FF0000" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" />
+                  <path d="M14.9283 6.32861L21.0144 12.3986L14.9283 18.4686" stroke="#FF0000" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+                  <path d="M3.96924 12.3979H20.8437" stroke="#FF0000" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
                 </svg>
               </div>
               <div className="pb-5 2xl:pb-[57.7px] px-30px pt-30px relative z-10">
--- app/components/home/sections/Services.tsx
+++ app/components/home/sections/Services.tsx
@@ -6,7 +6,7 @@ const Services = () => {
     return (
         <section className='py-150'>
             <div className='container'>
-                <div className='grid grid-cols-1 2xl:grid-cols-[auto_866.89px] 2xl:gap-[97.23px] text-white'>
+                <div className='grid grid-cols-1 2xl:grid-cols-[auto_866.89px] 2xl:gap-[97.23px] text-white gap-y-5'>
                     <div className='flex flex-col gap-[40px]'>
                         <h2 className='text-80 leading-[1.125]'>Our Services</h2>
                         <h4 className='text-30 leading-[1.333333333333333]'>At Quaddream, We offer Rentals and Sales to suit your requirement and Budget.</h4>
@@ -16,7 +16,7 @@ const Services = () => {
                         </p>
                     </div>
                     <div className='flex flex-col w-full gap-3 h-fit'>
-                        <div className='grid grid-cols-5 2xl:grid-rows-[336px_339px] gap-3'>
+                        <div className='grid grid-cols-2 xl:grid-cols-5 2xl:grid-rows-[336px_339px] gap-3'>
                             <div className='relative col-span-2 overflow-hidden rounded-2xl p-8 flex flex-col group'>
                                 <div className='absolute top-0 left-0 h-full w-full overflow-hidden'>
                                     <Image src="/assets/home/service-1.jpg" alt="Services" width={500} height={300} className='h-full w-full' />
@@ -31,8 +31,8 @@ const Services = () => {
                                 <div className='relative z-30 mt-auto'>
                                     <h3 className='text-33 leading-[1.212121212121212] mb-25px'>High-Quality Materials</h3>
                                     <Link href="/" className="flex items-center gap-2 group cursor-pointer">
-                                        <span>Explore</span> <svg width="19" height="15" viewBox="0 0 19 15" fill="none" xmlns="http://www.w3.org/2000/svg" 
-                                        className='group-hover:translate-x-2 transition-all duration-300'>
+                                        <span>Explore</span> <svg width="19" height="15" viewBox="0 0 19 15" fill="none" xmlns="http://www.w3.org/2000/svg"
+                                            className='group-hover:translate-x-2 transition-all duration-300'>
                                             <path d="M11.8843 1.2334L17.9543 7.3034L11.8843 13.3734" stroke="white" strokeWidth="1.5" strokeMiterlimit="10" strokeLinecap="round" strokeLinejoin="round" />
                                             <path d="M0.954102 7.30322H17.7841" stroke="white" strokeWidth="1.5" strokeMiterlimit="10" strokeLinecap="round" strokeLinejoin="round" />
                                         </svg>
@@ -53,8 +53,8 @@ const Services = () => {
                                 <div className='relative z-30 mt-auto'>
                                     <h3 className='text-33 leading-[1.212121212121212] mb-25px'>Cuplock Scaffolding & Aluminum Mobile Tower Rental & Sales</h3>
                                     <Link href="/" className="flex items-center gap-2 group cursor-pointer" >
-                                        <span>Explore</span> <svg width="19" height="15" viewBox="0 0 19 15" fill="none" xmlns="http://www.w3.org/2000/svg" 
-                                        className='group-hover:translate-x-2 transition-all duration-300'>
+                                        <span>Explore</span> <svg width="19" height="15" viewBox="0 0 19 15" fill="none" xmlns="http://www.w3.org/2000/svg"
+                                            className='group-hover:translate-x-2 transition-all duration-300'>
                                             <path d="M11.8843 1.2334L17.9543 7.3034L11.8843 13.3734" stroke="white" strokeWidth="1.5" strokeMiterlimit="10" strokeLinecap="round" strokeLinejoin="round" />
                                             <path d="M0.954102 7.30322H17.7841" stroke="white" strokeWidth="1.5" strokeMiterlimit="10" strokeLinecap="round" strokeLinejoin="round" />
                                         </svg>
@@ -76,9 +76,9 @@ const Services = () => {
                                 <div className='relative z-30 mt-auto'>
                                     <h3 className='text-33 leading-[1.212121212121212] mb-25px'>Scaffolding <br /> Formwork</h3>
                                     <Link href="/" className="flex items-center gap-2 group cursor-pointer">
-                                        <span>Explore</span> 
-                                        <svg width="19" height="15" viewBox="0 0 19 15" fill="none" xmlns="http://www.w3.org/2000/svg" 
-                                        className='group-hover:translate-x-2 transition-all duration-300'>
+                                        <span>Explore</span>
+                                        <svg width="19" height="15" viewBox="0 0 19 15" fill="none" xmlns="http://www.w3.org/2000/svg"
+                                            className='group-hover:translate-x-2 transition-all duration-300'>
                                             <path d="M11.8843 1.2334L17.9543 7.3034L11.8843 13.3734" stroke="white" strokeWidth="1.5" strokeMiterlimit="10" strokeLinecap="round" strokeLinejoin="round" />
                                             <path d="M0.954102 7.30322H17.7841" stroke="white" strokeWidth="1.5" strokeMiterlimit="10" strokeLinecap="round" strokeLinejoin="round" />
                                         </svg>
@@ -99,8 +99,8 @@ const Services = () => {
                                 <div className='relative z-30 mt-auto'>
                                     <h3 className='text-33 leading-[1.212121212121212] mb-25px'>Equipment Rentals</h3>
                                     <Link href="/" className="flex items-center gap-2 group cursor-pointer">
-                                        <span>Explore</span> <svg width="19" height="15" viewBox="0 0 19 15" fill="none" xmlns="http://www.w3.org/2000/svg" 
-                                        className='group-hover:translate-x-2 transition-all duration-300'>
+                                        <span>Explore</span> <svg width="19" height="15" viewBox="0 0 19 15" fill="none" xmlns="http://www.w3.org/2000/svg"
+                                            className='group-hover:translate-x-2 transition-all duration-300'>
                                             <path d="M11.8843 1.2334L17.9543 7.3034L11.8843 13.3734" stroke="white" strokeWidth="1.5" strokeMiterlimit="10" strokeLinecap="round" strokeLinejoin="round" />
                                             <path d="M0.954102 7.30322H17.7841" stroke="white" strokeWidth="1.5" strokeMiterlimit="10" strokeLinecap="round" strokeLinejoin="round" />
                                         </svg>
