a08156ca4c9cbfbaf6c95ba4bce377250f170803
rahees <raheez1995india@gmail.com>
2025-08-29 16:47:12 +0530

    project detail page

--- app/components/projects/data.ts
+++ app/components/projects/data.ts
@@ -1,97 +1,39 @@
- 
-  export const bannersection = {
-    data:[
-      {
+export const bannersection = {
+  data: [
+    {
       title: "Projects",
-      bgImg: "/assets/images/aboutus/banner.jpg", 
+      bgImg: "/assets/images/aboutus/banner.jpg",
       navigation: [
         { title: "Home", slug: "/" },
-        { title: "Projects", slug: "" }, 
+        { title: "Projects", slug: "" },
       ],
-    }
-  ]
-  };
- 
-  export const FeaturedPjtdata = {
-    data:[
-      {
-      heading: "Featured Projects", 
-      desc:"Discover our portfolio of innovative projects across various sectors, showcasing excellence in design and execution.",
+    },
+  ],
+};
+
+export const FeaturedPjtdata = {
+  data: [
+    {
+      heading: "Featured Projects",
+      desc: "Discover our portfolio of innovative projects across various sectors, showcasing excellence in design and execution.",
       items: [
-        { title:"Nakheel Mal", 
-          image:"/assets/images/projects/pjt1.jpg",
-           city: "Dubai", 
-           status: "Completed" ,
-           icon:"/assets/images/projects/location.svg",
-
-          },
-          { title:"DMCC - JLT", 
-            image:"/assets/images/projects/pjt2.jpg",
-             city: "Dubai", 
-             status: "Completed" ,
-             icon:"/assets/images/projects/location.svg",
-  
-            },
-            { title:"ADC – ABUDHABI", 
-              image:"/assets/images/projects/pjt3.jpg",
-               city: "Abu dhabi ", 
-               status: "Completed" ,
-               icon:"/assets/images/projects/location.svg",
-    
-              },
-              { title:"dubai Hills", 
-                image:"/assets/images/projects/pjt4.jpg",
-                 city: "Dubai ", 
-                 status: "Completed" ,
-                 icon:"/assets/images/projects/location.svg",
-      
-                },
-                { title:"Regalia Business Bay Project", 
-                  image:"/assets/images/projects/pjt5.jpg",
-                   city: "Dubai ", 
-                   status: "Completed" ,
-                   icon:"/assets/images/projects/location.svg",
-        
-                  },
-                { title:"City Night", 
-                  image:"/assets/images/projects/pjt6.jpg",
-                   city: "Dubai ", 
-                   status: "Completed" ,
-                   icon:"/assets/images/projects/location.svg",
-        
-                  },
-                  { title:"Sharjah Cement Factory", 
-                    image:"/assets/images/projects/pjt7.jpg",
-                     city: "Dubai ", 
-                     status: "Completed" ,
-                     icon:"/assets/images/projects/location.svg",
-          
-                    },
-                    { title:"EXPO 2020, Sweden Pavilion", 
-                      image:"/assets/images/projects/pjt8.jpg",
-                       city: "Dubai ", 
-                       status: "Completed" ,
-                       icon:"/assets/images/projects/location.svg",
-            
-                      }, 
-                        { title:"EXPO 2020, Switzerland Pavilion", 
-                          image:"/assets/images/projects/pjt9.jpg",
-                           city: "Dubai ", 
-                           status: "Completed" ,
-                           icon:"/assets/images/projects/location.svg",
-                
-                          },
-         
+        { title: "Nakheel Mal", image: "/assets/images/projects/pjt1.jpg", city: "Dubai", status: "Completed", icon: "/assets/images/projects/location.svg" },
+        { title: "DMCC - JLT", image: "/assets/images/projects/pjt2.jpg", city: "Dubai", status: "Completed", icon: "/assets/images/projects/location.svg" },
+        { title: "ADC – ABUDHABI", image: "/assets/images/projects/pjt3.jpg", city: "Abu dhabi ", status: "Completed", icon: "/assets/images/projects/location.svg" },
+        { title: "dubai Hills", image: "/assets/images/projects/pjt4.jpg", city: "Dubai ", status: "Completed", icon: "/assets/images/projects/location.svg" },
+        { title: "Regalia Business Bay Project", image: "/assets/images/projects/pjt5.jpg", city: "Dubai ", status: "Completed", icon: "/assets/images/projects/location.svg" },
+        { title: "City Night", image: "/assets/images/projects/pjt6.jpg", city: "Dubai ", status: "Completed", icon: "/assets/images/projects/location.svg" },
+        { title: "Sharjah Cement Factory", image: "/assets/images/projects/pjt7.jpg", city: "Dubai ", status: "Completed", icon: "/assets/images/projects/location.svg" },
+        { title: "EXPO 2020, Sweden Pavilion", image: "/assets/images/projects/pjt8.jpg", city: "Dubai ", status: "Completed", icon: "/assets/images/projects/location.svg" },
+        { title: "EXPO 2020, Switzerland Pavilion", image: "/assets/images/projects/pjt9.jpg", city: "Dubai ", status: "Completed", icon: "/assets/images/projects/location.svg" },
       ],
-    }
-  ]
-  };
-  export const ctaSection = {
-    title: "Have A Project In Mind?",
-    bgImg: "/assets/images/aboutus/cta-about.jpg", 
-    description: "Let's discuss how we can bring your vision to life. Our team of experts is ready to help you achieve your goals.",
-    buttonText: "Contact Us Now",
-    buttonLink: "",
-  };
-
- 
+    },
+  ],
+};
+export const ctaSection = {
+  title: "Have A Project In Mind?",
+  bgImg: "/assets/images/aboutus/cta-about.jpg",
+  description: "Let's discuss how we can bring your vision to life. Our team of experts is ready to help you achieve your goals.",
+  buttonText: "Contact Us Now",
+  buttonLink: "",
+};
