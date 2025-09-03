eeba89421621dbd124eda46a090cfb1380674869
Hisham I Globalsurf <hisham@globalsurf.ae>
2025-09-03 18:56:36 +0530

    Feat(Added animation to the blog project-details page)

--- app/components/project-details/sections/Highlights.tsx
+++ app/components/project-details/sections/Highlights.tsx
@@ -1,43 +1,70 @@
+"use client";
+
 import { projectDetails } from "@/app/components/project-details/data";
-import Image from "next/image"; 
- 
-   
-    const Highlights = () => { 
-     
+import Image from "next/image";
+import { motion } from "framer-motion";
+import { moveUp } from "../../motionVarients";
+
+const Highlights = () => {
   return (
-    <section className='py-150   relative bg-black  '>
-    <div className='container '>
+    <section className="py-150   relative bg-black  ">
+      <div className="container ">
         <div>
-            <h2 className='text-80 leading-[1.125] mb-5 md:mb-8  lg:mb-10 text-white  '>{projectDetails.project.highlights.title}</h2>
-            <p className='text-lite-gray text-19 leading-[1.684210526315789]   max-w-[75ch] mb-8 lg:mb-7'>{projectDetails.project.highlights.description}</p>
+          <motion.h2
+            variants={moveUp()}
+            initial="hidden"
+            whileInView="show"
+            viewport={{ once: true }}
+            className="text-80 leading-[1.125] mb-5 md:mb-8  lg:mb-10 text-white  "
+          >
+            {projectDetails.project.highlights.title}
+          </motion.h2>
+          <motion.p
+            variants={moveUp(0.2)}
+            initial="hidden"
+            whileInView="show"
+            viewport={{ once: true }}
+            className="text-lite-gray text-19 leading-[1.684210526315789]   max-w-[75ch] mb-8 lg:mb-7"
+          >
+            {projectDetails.project.highlights.description}
+          </motion.p>
         </div>
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:flex gap-10 lg:gap-5 2xl:gap-12">
-{projectDetails.project.highlights.items.map((item, index) => (
-<div key={index} className="group flex items-center gap-10 sm:gap-12">
-<div className="transition-all duration-300">
-<div className="flex  gap-3 sm:gap-7 items-center">
-  <div className="w-[40px] h-[40px] md:min-w-[67px] md:min-h-[67px] md:rounded-2xl rounded-sm bg-primary flex items-center justify-center">
-    <Image src={item.icon} alt={item.title} width={32} height={32} className='w-[22px] h-[24px] md:w-[32px] md:h-[32px]' />
-  </div>
-  <h3 className="relative text-19 xl:text-33 leading-[1.3] font-light text-white transition-all duration-300 max-w-[17ch]">
-    {item.title}
-  </h3>
-</div>
-</div>
+          {projectDetails.project.highlights.items.map((item, index) => (
+            <motion.div
+              variants={moveUp(index * 0.3)}
+              initial="hidden"
+              whileInView="show"
+              viewport={{ once: true }}
+              key={index}
+              className="group flex items-center gap-10 sm:gap-12"
+            >
+              <div className="transition-all duration-300">
+                <div className="flex  gap-3 sm:gap-7 items-center">
+                  <div className="w-[40px] h-[40px] md:min-w-[67px] md:min-h-[67px] md:rounded-2xl rounded-sm bg-primary flex items-center justify-center">
+                    <Image
+                      src={item.icon}
+                      alt={item.title}
+                      width={32}
+                      height={32}
+                      className="w-[22px] h-[24px] md:w-[32px] md:h-[32px]"
+                    />
+                  </div>
+                  <h3 className="self-end text-19 xl:text-33 leading-[1.3] font-light text-white transition-all duration-300 max-w-[17ch]">
+                    {item.title}
+                  </h3>
+                </div>
+              </div>
 
-{index < projectDetails.project.highlights.items.length - 1 && (
-<div className="hidden xl:block border-r border-[#BEBEBE] h-full w-[1px]" />
-)}
-</div>
-))}
-</div>
-
-
-        
-    </div> 
-    
-</section>
+              {index < projectDetails.project.highlights.items.length - 1 && (
+                <div className="hidden xl:block border-r border-[#BEBEBE] h-full w-[1px] mr-[20px] lg:mr-[30px] 2xl:mr-[70px]" />
+              )}
+            </motion.div>
+          ))}
+        </div>
+      </div>
+    </section>
   );
-}
+};
 
 export default Highlights;
--- app/components/project-details/sections/Main.tsx
+++ app/components/project-details/sections/Main.tsx
@@ -1,15 +1,32 @@
+"use client";
+
 import { projectDetails } from "@/app/components/project-details/data";
 import Image from "next/image";
 import { assets } from "@/public/assets/assets";
+import { motion } from "framer-motion";
+import { moveUp } from "../../motionVarients";
+
 const Main = () => {
   return (
     <section className="py-150 rounded-t-[20px] xl:rounded-tl-[40px] xl:rounded-tr-[40px] 2xl:rounded-tl-[80px] 2xl:rounded-tr-[80px] relative z-[50] mt-[-4.5%] bg-white">
       <div className="container">
-        <h2 className="text-80 leading-[1.125] mb-5 lg:mb-8 2xl:mb-30px text-black border-b border-lite-gray pb-5 md:pb-8 xl:pb-12">
+        <motion.h2
+          variants={moveUp(0)}
+          initial="hidden"
+          whileInView="show"
+          viewport={{ once: true }}
+          className="text-80 leading-[1.125] mb-5 lg:mb-8 2xl:mb-30px text-black border-b border-lite-gray pb-5 md:pb-8 xl:pb-12"
+        >
           {projectDetails.project.title}
-        </h2>
-        <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-4 items-center mb-6 md:mb-12 gap-y-5 lg:gap-y-0">
-          <div className="flex items-center gap-4 xl:gap-5">
+        </motion.h2>
+        <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-4 items-center justify-between mb-6 md:mb-12 gap-y-5 lg:gap-y-0">
+          <motion.div
+            variants={moveUp(0)}
+            initial="hidden"
+            whileInView="show"
+            viewport={{ once: true }}
+            className="flex items-center gap-4 xl:gap-5"
+          >
             <div className="flex items-center justify-center rounded-full border border-lite-gray w-10 h-10 xl:w-[78px] xl:h-[78px]">
               <Image
                 src={assets.clientIcon}
@@ -27,8 +44,14 @@ const Main = () => {
                 {projectDetails.project.client}
               </h4>
             </div>
-          </div>
-          <div className="flex items-center gap-4 xl:gap-5">
+          </motion.div>
+          <motion.div
+            variants={moveUp(0.1)}
+            initial="hidden"
+            whileInView="show"
+            viewport={{ once: true }}
+            className="flex items-center gap-4 xl:gap-5"
+          >
             <div className="flex items-center justify-center rounded-full border border-lite-gray w-10 h-10 xl:w-[78px] xl:h-[78px]">
               <Image
                 src={assets.statusIcon}
@@ -46,8 +69,14 @@ const Main = () => {
                 {projectDetails.project.status}
               </h4>
             </div>
-          </div>
-          <div className="flex items-center gap-4 xl:gap-5">
+          </motion.div>
+          <motion.div
+            variants={moveUp(0.3)}
+            initial="hidden"
+            whileInView="show"
+            viewport={{ once: true }}
+            className="flex items-center gap-4 xl:gap-5"
+          >
             <div className="flex items-center justify-center rounded-full border border-lite-gray w-10 h-10 xl:w-[78px] xl:h-[78px]">
               <Image
                 src={assets.sectorIcon}
@@ -65,8 +94,14 @@ const Main = () => {
                 {projectDetails.project.sector}
               </h4>
             </div>
-          </div>
-          <div className="flex items-center gap-4 xl:gap-5">
+          </motion.div>
+          <motion.div
+            variants={moveUp(0.4)}
+            initial="hidden"
+            whileInView="show"
+            viewport={{ once: true }}
+            className="flex items-center gap-4 xl:gap-5"
+          >
             <div className="flex items-center justify-center rounded-full border border-lite-gray w-10 h-10 xl:w-[78px] xl:h-[78px]">
               <Image
                 src={assets.locationIcon}
@@ -84,40 +119,75 @@ const Main = () => {
                 {projectDetails.project.location}
               </h4>
             </div>
-          </div>
+          </motion.div>
         </div>
-        <Image
-          src={projectDetails.project.image}
-          alt=""
-          width={1920}
-          height={1280}
-          className="object-cover w-full mb-5 lg:mb-8 2xl:mb-50px h-[260px] md:h-[400px] xl:h-[601px] rounded-2xl"
-        />
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-5 lg:gap-8 2xl:gap-25">
+        <motion.div
+          variants={moveUp(0.2)}
+          initial="hidden"
+          whileInView="show"
+          viewport={{ once: true }}
+        >
+          <Image
+            src={projectDetails.project.image}
+            alt=""
+            width={1920}
+            height={1280}
+            className="object-cover w-full mb-5 lg:mb-8 2xl:mb-50px h-[260px] md:h-[400px] xl:h-[601px] rounded-2xl"
+          />
+        </motion.div>
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-5 lg:gap-12 2xl:gap-25">
           <div>
-            <h2 className="text-80 leading-[1.125] text-black mb-5 md:mb-8 xl:mb-12">
+            <motion.h2
+              variants={moveUp()}
+              initial="hidden"
+              whileInView="show"
+              viewport={{ once: true }}
+              className="text-80 leading-[1.125] text-black mb-5 md:mb-8 xl:mb-12"
+            >
               Project Description
-            </h2>
-            <p className="text-19 leading-lh-text19 text-gray-para">
+            </motion.h2>
+            <motion.p
+              variants={moveUp(0.3)}
+              initial="hidden"
+              whileInView="show"
+              viewport={{ once: true }}
+              className="text-19 leading-lh-text19 text-gray-para"
+            >
               {projectDetails.project.description}
-            </p>
+            </motion.p>
           </div>
           <div className="mt-0 lg:mt-5">
-            <h3 className="text-30 leading-text25 text-black mb-5 md:mb-8 xl:mb-12 border-b border-lite-gray pb-4">
+            <motion.h3
+              variants={moveUp()}
+              initial="hidden"
+              whileInView="show"
+              viewport={{ once: true }}
+              className="text-30 leading-text25 text-black mb-5 md:mb-8 xl:mb-12 border-b border-lite-gray pb-4"
+            >
               Services Delivered
-            </h3>
-            <ul className="list-disc list-inside marker:text-primary">
+            </motion.h3>
+            <motion.ul
+              variants={moveUp(0.2)}
+              initial="hidden"
+              whileInView="show"
+              viewport={{ once: true }}
+              className="list-disc list-inside marker:text-primary"
+            >
               {projectDetails.project.servicesDelivered.map(
                 (service, index) => (
-                  <li
+                  <motion.li
+                    variants={moveUp(index * 0.2)}
+                    initial="hidden"
+                    whileInView="show"
+                    viewport={{ once: true }}
                     key={index}
-                    className="text-19 leading-lh-text19 text-gray-para "
+                    className="text-18 leading-[1.7] text-gray-para "
                   >
                     {service}
-                  </li>
+                  </motion.li>
                 )
               )}
-            </ul>
+            </motion.ul>
           </div>
         </div>
       </div>
