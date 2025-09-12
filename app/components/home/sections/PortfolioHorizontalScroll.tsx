7ab46114d7d173098b2860e016621921affc9142
Hisham I Globalsurf <hisham@globalsurf.ae>
2025-09-12 15:33:38 +0530

    fix(Portfolio items count): render only 5 items in portfolio slider in home page

--- app/components/home/sections/PortfolioHorizontalScroll.tsx
+++ app/components/home/sections/PortfolioHorizontalScroll.tsx
@@ -1,22 +1,22 @@
-'use client';
-import React, { useEffect, useRef } from 'react';
-import Image from 'next/image';
-import Link from 'next/link';
-import gsap from 'gsap';
-import { ScrollTrigger } from 'gsap/ScrollTrigger';
-import { motion } from 'motion/react';
-import { moveUp, containerStagger, paragraphItem } from '../../motionVarients';
+"use client";
+import React, { useEffect, useRef } from "react";
+import Image from "next/image";
+import Link from "next/link";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { motion } from "motion/react";
+import { moveUp, containerStagger, paragraphItem } from "../../motionVarients";
 
 gsap.registerPlugin(ScrollTrigger);
 
 import { Projects } from "../../projects/type";
 
-type ProjectSwiperProps = { 
+type ProjectSwiperProps = {
   projectsdata: Projects;
 };
 
-const PortfolioHorizontalScroll: React.FC<ProjectSwiperProps> = ({  
-  projectsdata 
+const PortfolioHorizontalScroll: React.FC<ProjectSwiperProps> = ({
+  projectsdata,
 }) => {
   // Access the projects array from the projectsdata object
   const projects = projectsdata.projects || [];
@@ -25,33 +25,32 @@ const PortfolioHorizontalScroll: React.FC<ProjectSwiperProps> = ({
   const horizontalRef = useRef<HTMLDivElement | null>(null);
 
   useEffect(() => {
-  const section = sectionRef.current;
-  const horizontal = horizontalRef.current;
+    const section = sectionRef.current;
+    const horizontal = horizontalRef.current;
 
-  if (!section || !horizontal) return;
+    if (!section || !horizontal) return;
 
-  const totalScrollWidth = horizontal.scrollWidth;
-  const viewportWidth = window.innerWidth;
-  const scrollDistance = totalScrollWidth - viewportWidth;
+    const totalScrollWidth = horizontal.scrollWidth;
+    const viewportWidth = window.innerWidth;
+    const scrollDistance = totalScrollWidth - viewportWidth;
 
-  gsap.to(horizontal, {
-    x: -scrollDistance,
-    ease: 'none',
-    scrollTrigger: {
-      trigger: section,
-      start: 'top top',
-      end: () => `+=${scrollDistance}`,
-      pin: true,
-      scrub: true, // no delay
-      anticipatePin: 1,
-    },
-  });
+    gsap.to(horizontal, {
+      x: -scrollDistance,
+      ease: "none",
+      scrollTrigger: {
+        trigger: section,
+        start: "top top",
+        end: () => `+=${scrollDistance}`,
+        pin: true,
+        scrub: true, // no delay
+        anticipatePin: 1,
+      },
+    });
 
-  return () => {
-    ScrollTrigger.getAll().forEach(trigger => trigger.kill());
-  };
-}, [projects]);
-
+    return () => {
+      ScrollTrigger.getAll().forEach((trigger) => trigger.kill());
+    };
+  }, [projects]);
 
   return (
     <section ref={sectionRef} className="bg-white py-150 overflow-hidden">
@@ -65,87 +64,84 @@ const PortfolioHorizontalScroll: React.FC<ProjectSwiperProps> = ({
           viewport={{ amount: 0.1, once: true }}
         >
           Portfolio
-        </motion.h2> 
-          <motion.div
-            variants={moveUp(0.5)}
-            initial="hidden"
-            whileInView="show"
-            transition={{ duration: 0.6 }}
-            viewport={{ amount: 0.1, once: true }}
+        </motion.h2>
+        <motion.div
+          variants={moveUp(0.5)}
+          initial="hidden"
+          whileInView="show"
+          transition={{ duration: 0.6 }}
+          viewport={{ amount: 0.1, once: true }}
+        >
+          <Link
+            href={"/projects"}
+            className="flex items-center gap-2 cursor-pointer text-16 border border-black py-[5px] md:py-[10px] px-[10px] md:px-[20px] rounded-[60px] w-fit z-10 group font-normal"
           >
-            <Link
-              href={'/projects'}
-              className="flex items-center gap-2 cursor-pointer text-16 border border-black py-[5px] md:py-[10px] px-[10px] md:px-[20px] rounded-[60px] w-fit z-10 group font-normal"
-            >
-              <span>View Projects</span>
-              <span className="bg-primary w-8 h-8 md:w-[51.7px] md:h-[51.7px] flex items-center justify-center rounded-full group-hover:translate-x-[10px] transition-all duration-300">
-                <Image
-                  src="/assets/images/home/arrow-right.svg"
-                  alt="Arrow"
-                  width={24}
-                  height={24}
-                  className='w-4 h-4 md:w-[24px] md:h-[24px]'
-                />
-              </span>
-            </Link>
-          </motion.div>
-      
-      </div>
-        <div className="container">
-      <div
-        ref={horizontalRef}
-        className="flex gap-6 will-change-transform"
-        style={{ width: `${projects.length * 44}vw` }}
-      >
-        {projects.map(project => (
-          <Link href={`/projects/${project.slug}`} key={project.slug}>
-            <div 
-            className="relative rounded-[12px] overflow-hidden group shadow-lg h-[300px] w-[350px] lg:h-[500px] 2xl:h-[542px] lg:w-[630px] 2xl:w-[757.67px] flex-shrink-0"
-          >
-            <Image
-              src={project.thumbnail}
-              alt={project.thumbnailAlt || project.firstSection.title}
-              fill
-              className="object-cover transition-transform duration-300 group-hover:scale-105"
-            />
-            <div className='relative z-10 h-full w-full'>
-            <div className="absolute top-[33px] left-[43px] bg-[#fafafa70] px-[10px] py-[11px] rounded-full text-19 font-light flex items-center backdrop-blur-[18px] w-[250px] h-[53px]">
+            <span>View Projects</span>
+            <span className="bg-primary w-8 h-8 md:w-[51.7px] md:h-[51.7px] flex items-center justify-center rounded-full group-hover:translate-x-[10px] transition-all duration-300">
               <Image
-                src="/assets/images/home/portfolio/location.svg"
-                width={30}
-                height={30}
-                alt=""
-                className="mr-[15px]"
+                src="/assets/images/home/arrow-right.svg"
+                alt="Arrow"
+                width={24}
+                height={24}
+                className="w-4 h-4 md:w-[24px] md:h-[24px]"
               />
-              {project.firstSection.location?.name || ''}
-            </div>
-            <motion.div
-              className="absolute bottom-0 left-0 right-0 p-6  to-transparent text-white"
-              variants={containerStagger}
-              initial="hidden"
-              whileInView="show"
-              transition={{ duration: 0.6 }}
-              viewport={{ amount: 0.1, once: false }}
-            >
-              <motion.h3
-                className="text-33 leading-[1.2] capitalize"
-                variants={paragraphItem}
-              >
-                {project.firstSection.title}
-              </motion.h3>
-              <motion.p
-                className="text-33 leading-[1.2] capitalize"
-                variants={paragraphItem}
-              >
-                {project.firstSection.location?.name || ''}
-              </motion.p>
-            </motion.div>
-            </div>
-            <div className="absolute h-full w-full z-[1] bottom-0 left-0 right-0 p-6 bg-gradient-to-t from-black via-black/20 to-transparent text-white" ></div>
-          </div>
+            </span>
           </Link>
-        ))}
+        </motion.div>
       </div>
+      <div className="container">
+        <div
+          ref={horizontalRef}
+          className="flex gap-6 will-change-transform"
+          style={{ width: `${5 * 44}vw` }}
+        >
+          {projects.slice(0, 5).map((project) => (
+            <Link href={`/projects/${project.slug}`} key={project.slug}>
+              <div className="relative rounded-[12px] overflow-hidden group shadow-lg h-[300px] w-[350px] lg:h-[500px] 2xl:h-[542px] lg:w-[630px] 2xl:w-[757.67px] flex-shrink-0">
+                <Image
+                  src={project.thumbnail}
+                  alt={project.thumbnailAlt || project.firstSection.title}
+                  fill
+                  className="object-cover transition-transform duration-300 group-hover:scale-105"
+                />
+                <div className="relative z-10 h-full w-full">
+                  <div className="absolute top-[33px] left-[43px] bg-[#fafafa70] px-[10px] py-[11px] rounded-full text-19 font-light flex items-center backdrop-blur-[18px] w-[250px] h-[53px]">
+                    <Image
+                      src="/assets/images/home/portfolio/location.svg"
+                      width={30}
+                      height={30}
+                      alt=""
+                      className="mr-[15px]"
+                    />
+                    {project.firstSection.location?.name || ""}
+                  </div>
+                  <motion.div
+                    className="absolute bottom-0 left-0 right-0 p-6  to-transparent text-white"
+                    variants={containerStagger}
+                    initial="hidden"
+                    whileInView="show"
+                    transition={{ duration: 0.6 }}
+                    viewport={{ amount: 0.1, once: false }}
+                  >
+                    <motion.h3
+                      className="text-33 leading-[1.2] capitalize"
+                      variants={paragraphItem}
+                    >
+                      {project.firstSection.title}
+                    </motion.h3>
+                    <motion.p
+                      className="text-33 leading-[1.2] capitalize"
+                      variants={paragraphItem}
+                    >
+                      {project.firstSection.location?.name || ""}
+                    </motion.p>
+                  </motion.div>
+                </div>
+                <div className="absolute h-full w-full z-[1] bottom-0 left-0 right-0 p-6 bg-gradient-to-t from-black via-black/20 to-transparent text-white"></div>
+              </div>
+            </Link>
+          ))}
+        </div>
       </div>
     </section>
   );
