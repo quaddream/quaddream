14728cb500e4f130ee4965cffcbc57c8bb887cbf
hisham-globalsurf <hisham@globalsurf.ae>
2026-01-23 17:12:24 +0530

    feat(metaData, metaDescription)

--- app/(user)/downloads/page.tsx
+++ app/(user)/downloads/page.tsx
@@ -1,5 +1,26 @@
 import React from "react";
 import Index from "@/app/components/downloads/Index";
+import { Metadata } from "next";
+
+export async function generateMetadata(): Promise<Metadata> {
+  const response = await fetch(`${process.env.BASE_URL}/api/admin/downloads`, { next: { revalidate: 60 } });
+  const data = await response.json();
+
+  const metadataTitle = data?.data?.metaTitle || "Quad Dream";
+  const metadataDescription =
+    data?.data?.metaDescription || "Quad Dream";
+
+  return {
+    title: metadataTitle,
+    description: metadataDescription,
+    openGraph: {
+      title: metadataTitle,
+      description: metadataDescription,
+      url: process.env.BASE_URL,
+      siteName: "Quad Dream",
+    },
+  };
+}
 
 export default async function Downloads() {
   const response = await fetch(`${process.env.BASE_URL}/api/admin/downloads`, {
